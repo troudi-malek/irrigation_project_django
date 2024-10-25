4a7357dde12888014514444d4c2dae72916dcddc
AmineAbdessayed <mohamedamine.abdessayed@esprit.tn>
2024-10-25 15:24:47 +0100

    Dashbord

--- soilCropRecommender/urls.py
+++ soilCropRecommender/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('client',views.index),
+    path('amine/',views.amine)
+
 ]
--- soilCropRecommender/views.py
+++ soilCropRecommender/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html',{})
 
+
+def amine(request):
+    return render(request,'Admin/index.html',{})
+
+
 # Create your views here.
